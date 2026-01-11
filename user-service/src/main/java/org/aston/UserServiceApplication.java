31ad4213719d163a2f6db1d6fc627ec1fdb397f0
Alexander Vereta <alexander.git@yandex.ru>
2026-01-11 19:13:35 +0500

    add docker profiles and add .gitignore to congif-service

--- user-service/src/main/java/org/aston/UserServiceApplication.java
+++ user-service/src/main/java/org/aston/UserServiceApplication.java
@@ -11,7 +11,6 @@ public class UserServiceApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 
